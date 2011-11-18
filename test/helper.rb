f13ad5112c4cf4a2e2374eaa2191d323fc64021a
TAGOMORI Satoshi <tagomoris@gmail.com>
2011-11-18 15:47:12 +0900

    fix out_hoop module name

--- test/helper.rb
+++ test/helper.rb
@@ -12,7 +12,7 @@ require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'fluent-plugin-hoop'
+require 'fluent/plugin/out_hoop'
 
 class Test::Unit::TestCase
 end
