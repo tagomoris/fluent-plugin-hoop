ebd10b8bdda294193e3f36bace935c2bc45852fa
TAGOMORI Satoshi <tagomoris@gmail.com>
2011-11-25 13:03:05 +0900

    wrote dummy server initialization, and configuration tests

--- test/plugin/test_out_hoop.rb
+++ test/plugin/test_out_hoop.rb
@@ -1,36 +1,112 @@
 require 'helper'
 # require 'time'
+require 'webrick'
 
 class HoopOutputTest < Test::Unit::TestCase
   # TMP_DIR = File.dirname(__FILE__) + "/../tmp"
 
   def setup
     Fluent::Test.setup
-    # FileUtils.rm_rf(TMP_DIR)
-    # FileUtils.mkdir_p(TMP_DIR)
+    @dummy_server_thread = Thread.new do
+      srv = WEBrick::HTTPServer.new({:BindAddress => '127.0.0.1', :Port => 14000, :AccessLog => [[$log,'']]})
+      srv.mount_proc('/logs/from/fluentd') {|req, res|
+        # hogehoge
+      }
+      begin
+        srv.start
+      ensure
+        srv.shutdown
+      end
+    end
+    Thread.pass
   end
 
+  def teardown
+    @dummy_server_thread.kill
+    @dummy_server_thread.join
+  end
+
+  # include Fluent::SetTagKeyMixin
+  # config_set_default :include_tag_key, false
+
+  # include Fluent::SetTimeKeyMixin
+  # config_set_default :include_time_key, true
+
+  # config_param :hoop_server, :string   # host:port
+  # config_param :path, :string          # /path/pattern/to/hdfs/file can use %Y %m %d %H %M %S and %T(tag, not-supported-yet)
+  # config_param :username, :string      # hoop pseudo username
+  
+  # config_param :time_format, :string, :default => nil
+  # config_param :output_type, :string, :default => 'json' # or 'attr:field' or 'attributes:field1,field2,field3(...)'
+  # config_param :add_newline, :bool,   :default => false
+  # config_param :field_separator, :string, :default => 'TAB' # or SPACE,COMMA (for output_type=attributes:*)
+
+
   CONFIG = %[
+    hoop_server localhost:14000
+    path /logs/from/fluentd/foo-%Y%m%d%H
+    username hoopuser
   ]
-  # CONFIG = %[
-  #   path #{TMP_DIR}/out_file_test
-  #   compress gz
-  #   utc
-  # ]
 
   def create_driver(conf = CONFIG)
     Fluent::Test::BufferedOutputTestDriver.new(Fluent::HoopOutput).configure(conf)
   end
 
   def test_configure
-    #### set configurations
-    # d = create_driver %[
-    #   path test_path
-    #   compress gz
-    # ]
-    #### check configurations
-    # assert_equal 'test_path', d.instance.path
-    # assert_equal :gz, d.instance.compress
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver %[
+      ]
+    }
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver %[
+    path /logs/from/fluentd/foo-%Y%m%d%H
+    username hoopuser
+      ]
+    }
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver %[
+    hoop_server hoop.master.local:14000
+    username hoopuser
+      ]
+    }
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver %[
+    hoop_server hoop.master.local:14000
+    path /logs/from/fluentd/foo-%Y%m%d%H
+      ]
+    }
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver %[
+    hoop_server hoop.master.local
+    path /logs/from/fluentd/foo-%Y%m%d%H
+    username hoopuser
+      ]
+    }
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver %[
+    hoop_server hoop.master.local:xxx
+    path /logs/from/fluentd/foo-%Y%m%d%H
+    username hoopuser
+      ]
+    }
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver %[
+    hoop_server hoop.master.local:xxx
+    path logs/from/fluentd/foo-%Y%m%d%H
+    username hoopuser
+      ]
+    }
+
+    d = create_driver(CONFIG)
+    
+    assert_equal d.instance.hoop_server, 'hoop.master.local:14000'
+    assert_equal d.instance.path, '/logs/from/fluentd/foo-%Y%m%d%H'
+    assert_equal d.instance.username, 'hoopuser'
+
+    assert_nil d.instance.time_format
+    assert_equal d.instance.output_type, 'json'
+    assert_equal d.instance.add_newline, false
+    assert_equal d.instance.field_separator, 'TAB'
   end
 
   def test_format
