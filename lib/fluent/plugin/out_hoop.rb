9cfa8d6cb77b3664d82cdf8365577c9cee1aafd7
TAGOMORI Satoshi <tagomoris@gmail.com>
2011-11-25 20:02:43 +0900

    wrote configurations for format method

--- lib/fluent/plugin/out_hoop.rb
+++ lib/fluent/plugin/out_hoop.rb
@@ -3,19 +3,18 @@ class Fluent::HoopOutput < Fluent::TimeSlicedOutput
 
   config_set_default :buffer_type, 'memory'
 
-  include Fluent::SetTagKeyMixin
-  config_set_default :include_tag_key, false
-
-  include Fluent::SetTimeKeyMixin
-  config_set_default :include_time_key, true
-
   config_param :hoop_server, :string   # host:port
   config_param :path, :string          # /path/pattern/to/hdfs/file can use %Y %m %d %H %M %S and %T(tag, not-supported-yet)
   config_param :username, :string      # hoop pseudo username
   
+  config_set_default :utc, true
+  config_set_default :localtime, false
+
+  config_param :output_time, :bool, :default => true
   config_param :time_format, :string, :default => nil
-  config_param :output_type, :string, :default => 'json' # or 'attr:field' or 'attributes:field1,field2,field3(...)'
-  config_param :add_newline, :bool,   :default => false
+  config_param :output_tag, :bool, :default => true
+  config_param :output_type, :string, :default => 'json' # or 'attr:field' or 'attr:field1,field2,field3(...)'
+  config_param :add_newline, :bool,   :default => true
   config_param :field_separator, :string, :default => 'TAB' # or SPACE,COMMA (for output_type=attributes:*)
   # config_param :tag_format, :string, :default => 'all' # or 'last'(last.part.of.tag => tag) or 'none'
 
@@ -26,23 +25,85 @@ class Fluent::HoopOutput < Fluent::TimeSlicedOutput
 
   def configure(conf)
     super
-    unless /\A([a-zA-Z0-9][-a-zA-Z0-9.]*):(\d+)\Z/ =~ conf['hoop_server']
-      raise Fluent::ConfigError, "Invalid config value on hoop_server: '#{conf['hoop_server']}', needs SERVER_NAME:PORT"
+
+    unless /\A([a-zA-Z0-9][-a-zA-Z0-9.]*):(\d+)\Z/ =~ @hoop_server
+      raise Fluent::ConfigError, "Invalid config value on hoop_server: '#{@hoop_server}', needs SERVER_NAME:PORT"
     end
     @host = $1
     @port = $2.to_i
-    @path = conf['path']
     unless @path.index('/') == 0
-      raise Fluent::ConfigError, "Path on hdfs MUST starts with '/', but '#{conf['path']}'"
+      raise Fluent::ConfigError, "Path on hdfs MUST starts with '/', but '#{@path}'"
     end
-    @username = conf['username']
     @conn = nil
     @header = {'Content-Type' => 'application/octet-stream'}
+
+    @f_separator = case @field_separator
+                   when 'SPACE' then ' '
+                   when 'COMMA' then ','
+                   else "\t"
+                   end
+
+    if @utc
+      @localtime = false
+    end
+    @timef = @output_time ? Fluent::TimeFormatter.new(@time_format, @localtime) : nil
+
+    @line_end = @add_newline ? "\n" : ""
+
+    # config_param :output_type, :string, :default => 'json' # or 'attr:field' or 'attr:field1,field2,field3(...)'
+    @custom_attributes = []
+    if @output_type == 'json'
+      # default record_to_string
+    elsif @output_type =~ /^attr:(.*)$/
+      @custom_attributes = $1.split(',')
+      if @custom_attributes.size > 1
+        self.instance_eval {
+          def record_to_string(record); @custom_attributes.map{|attr| record[attr]}.join($f_separator); end
+        }
+      elsif @custom_attributes.size == 1
+        self.instance_eval { def record_to_string(record); record[@custom_attributes[0]]; end }
+      else
+        raise Fluent::ConfigError, "Invalid attributes specification: '#{@output_type}', needs one or more attributes."
+      end
+    else
+      raise Fluent::ConfigError, "Invalid output_type: '#{@output_type}'. specify 'json' or 'attr:ATTRIBUTE_NAME' or 'attr:ATTR1,ATTR2,...'"
+    end
+
+    if @output_time and @output_tag
+      # default format method
+    elsif @output_time
+      self.instance_eval {
+        def format(time,tag,record);
+          time_str = @timef.format(time) ; time_str + @f_separator + record_to_string(record) + @line_end
+        end
+      }
+    elsif @output_tag
+      self.instance_eval {
+        def format(time,tag,record);
+          tag + @f_separator + record_to_string(record) + @line_end
+        end
+      }
+    else
+      if @add_newline
+        self.instance_eval {
+          def format(time,tag,record);
+            record_to_string(record) + @line_end
+          end
+        }
+      else
+        self.instance_eval {
+          def format(time,tag,record);
+            record_to_string(record)
+          end
+        }
+      end
+    end
   end
 
   def start
     super
     @conn = Net::HTTP.start(@host, @port)
+    # client.request
     $log.info "connected hoop server: #{@host} port #{@port}"
   end
 
@@ -51,8 +112,32 @@ class Fluent::HoopOutput < Fluent::TimeSlicedOutput
     @conn.finish
   end
 
+  def record_to_string(record)
+    record.to_json
+  end
+
+  def format_fullspec(tag, time, record)
+    buf = ''
+    if @output_time
+      buf += @timef.format(time) + @f_separator
+    end
+    if @output_tag
+      buf += tag + @f_separator
+    end
+    if @output_type == 'json'
+      buf += record.json
+    else
+      buf += @custom_attributes.map{|attr| record[attr]}.join(@f_separator)
+    end
+    if @add_newline
+      buf += "\n"
+    end
+    buf
+  end
+
   def format(tag, time, record)
-    [tag, time, record].to_msgpack
+    time_str = @timef.format(time)
+    time_str + @f_separator + tag + @f_separator + record_to_string(record) + (@add_newline ? "\n" : '')
   end
 
   def write(chunk)
