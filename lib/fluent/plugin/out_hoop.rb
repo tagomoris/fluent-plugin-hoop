fc6dba597516c1591308d404acf43e873fa9e6ab
TAGOMORI Satoshi <tagomoris@gmail.com>
2011-12-22 20:01:13 +0900

    rewrite http retrying

--- lib/fluent/plugin/out_hoop.rb
+++ lib/fluent/plugin/out_hoop.rb
@@ -295,39 +295,45 @@ class Fluent::HoopOutput < Fluent::TimeSlicedOutput
     Time.strptime(chunk_key, @time_slice_format).strftime(@path)
   end
 
+  def send_data(path, data, retries=0)
+    conn = Net::HTTP.start(@host, @port)
+    conn.read_timeout = 5
+    res = conn.request_put(path + "?op=append", data, @authorized_header)
+    if res.code == '401'
+      res = conn.request_get("/?op=status&user.name=#{@username}")
+      if res.code.to_i < 300 and res['Set-Cookie']
+        @authorized_header = {'Cookie' => res['Set-Cookie'].split(';')[0], 'Content-Type' => 'application/octet-stream'}
+      else
+        $log.error "Failed to update authorized cookie, code: #{res.code}, message: #{res.body}"
+        raise Fluent::ConfigError, "Failed to update authorized cookie, code: #{res.code}, message: #{res.body}"
+      end
+      res = conn.request_put(hdfs_path + "?op=append", data, @authorized_header)
+    end
+    if res.code == '404'
+      res = conn.request_post(path + "?op=create&overwrite=false", data, @authorized_header)
+    end
+    if res.code == '500'
+      if retries >= 3
+        raise StandardError, "failed to send_data with retry 3 times InternalServerError"
+      end
+      sleep 0.3 # yes, this is a magic number
+      res = send_data(path, data, retries + 1)
+    end
+    conn.finish
+    if res.code != '200' and res.code != '201'
+      $log.warn "failed to write data to path: #{path}, code: #{res.code} #{res.message}"
+    end
+    res
+  end
+
   def write(chunk)
     hdfs_path = path_format(chunk.key)
-    conn = Net::HTTP.start(@host, @port)
     begin
-      res = conn.request_put(hdfs_path + "?op=append", chunk.read, @authorized_header)
-      if res.code == '401'
-        res = conn.request_get("/?op=status&user.name=#{@username}")
-        if res.code.to_i < 300 and res['Set-Cookie']
-          @authorized_header = {'Cookie' => res['Set-Cookie'].split(';')[0], 'Content-Type' => 'application/octet-stream'}
-        else
-          $log.error "Failed to update authorized cookie, code: #{res.code}, message: #{res.body}"
-          raise Fluent::ConfigError, "Failed to update authorized cookie, code: #{res.code}, message: #{res.body}"
-        end
-        res = conn.request_put(hdfs_path + "?op=append", chunk.read, @authorized_header)
-      end
-      if res.code == '404'
-        res = conn.request_post(hdfs_path + "?op=create&overwrite=false", chunk.read, @authorized_header)
-        if res.code == '500'
-          res = conn.request_put(hdfs_path + "?op=append", chunk.read, @authorized_header)
-        end
-      end
-      if res.code != '200' and res.code != '201'
-        $log.warn "failed to write data to path: #{hdfs_path}, code: #{res.code} #{res.message}"
-      else
-        if res['Set-Cookie']
-          @authorized_header['Cookie'] = (res['Set-Cookie'] || '').split(';')[0]
-        end
-      end
+      send_data(hdfs_path, chunk.read)
     rescue
       $log.error "failed to communicate server, #{@host} port #{@port}, path: #{hdfs_path}"
       raise
     end
-    conn.finish
     hdfs_path
   end
 end
